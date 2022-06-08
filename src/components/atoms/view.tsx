086952198f670e58c668a72a62939de66d265348
hhyemi <dlgpal9508@gmail.com>
2022-06-08 21:22:43 +0900

    add view component

---
+++ src/components/atoms/view.tsx
@@ -0,0 +1,6 @@
+import { space, layout, typography, color, position, border, alignSelf, flex } from "styled-system";
+import styled from "styled-components/native";
+
+const View = styled.View(space, layout, typography, color, position, border, alignSelf, flex);
+
+export default View;

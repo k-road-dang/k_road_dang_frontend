734d0da8ee1a0ab9e06dc87e1beb8e69d611bd4d
hhyemi <dlgpal9508@gmail.com>
2022-06-21 12:16:29 +0900

    add divider,avatar components

---
+++ src/components/atoms/avatar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ImageSourcePropType } from "react-native";
+import styled from "styled-components/native";
+import { width as styledWidth, height as styledHeight, space, SpaceProps } from "styled-system";
+
+export type AvatarProps = SpaceProps & {
+  source: ImageSourcePropType;
+  size: number;
+};
+
+const StyledAvatar = styled.Image({ borderRadius: "50px" }, space, styledWidth, styledHeight);
+
+export const Avatar: React.FunctionComponent<AvatarProps> = ({ source, size, ...rest }) => {
+  return <StyledAvatar source={source} width={size} height={size} {...rest} />;
+};
